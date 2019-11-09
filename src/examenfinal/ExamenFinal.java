822837dc4c97fb2c1a2044c25e51db28dbdf25ef
Rodrigo Carcuz <carcuz789@gmail.com>
2019-11-09 16:56:46 -0600

    commit2

--- src/examenfinal/ExamenFinal.java
+++ src/examenfinal/ExamenFinal.java
@@ -5,6 +5,11 @@
  */
 package examenfinal;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 /**
  *
  * @author Rodrigo Carcuz
@@ -16,6 +21,79 @@ public int[] VectorArb= new int[50];
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       Scanner sn = new Scanner(System.in);
+       Scanner sn2 = new Scanner(System.in);
+       boolean salir = false;
+       int opcion; //Guardaremos la opcion del usuario
+        
+       while(!salir){
+            
+           System.out.println("1. carga masiva");
+           System.out.println("2. reportes");
+           System.out.println("3. carga manual");
+           System.out.println("4. Salir");
+            
+           System.out.println("Escribe una de las opciones");
+           opcion = sn.nextInt();
+            
+           switch(opcion){
+               case 1:
+                   
+                   System.out.println("ingrese la ruta del archivo \n");
+                   String Ruta = sn2.nextLine();
+                    BufferedReader br = null;
+        try {
+           //Crear un objeto BufferedReader al que se le pasa 
+           //   un objeto FileReader con el nombre del fichero
+           br = new BufferedReader(new FileReader(Ruta));
+           //Leer la primera línea, guardando en un String
+           String texto = br.readLine();
+           //Repetir mientras no se llegue al final del fichero
+           while(texto != null)
+           {
+               //Hacer lo que sea con la línea leída
+               
+               String[] Var=texto.split(",");        
+               
+           
+               texto = br.readLine();
+           }
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error: Fichero no encontrado");
+            System.out.println(e.getMessage());
+        }
+        catch(Exception e) {
+            System.out.println("Error de lectura del fichero");
+            System.out.println(e.getMessage());
+        }
+        finally {
+            try {
+                if(br != null)
+                    br.close();
+            }
+            catch (Exception e) {
+                System.out.println("Error al cerrar el fichero");
+                System.out.println(e.getMessage());
+            }
+        }
+                   
+                   
+                   break;
+               case 2:
+                   System.out.println("Has seleccionado la opcion 2");
+                   break;
+                case 3:
+                   System.out.println("Has seleccionado la opcion 3");
+                   break;
+                case 4:
+                   salir=true;
+                   break;
+                default:
+                   System.out.println("Solo números entre 1 y 4");
+           }
+            
+       }   
         
     }
     
