c04b0671d41d9b35cef6b3f3715d61ca04420fb3
Rodrigo Carcuz <carcuz789@gmail.com>
2019-11-09 16:20:14 -0600

    commit1

---
+++ src/examenfinal/ExamenFinal.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenfinal;
+
+/**
+ *
+ * @author Rodrigo Carcuz
+ */
+public class ExamenFinal {
+public int[] VectorArb= new int[50];
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
